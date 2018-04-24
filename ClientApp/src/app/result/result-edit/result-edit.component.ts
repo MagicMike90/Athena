a61b69c8bbd7ef6277b9851a4617936db110e403
MagicMike90 <lcw303661398@hotmail.com>
2018-04-25 00:14:48 +1000

    update result module

--- ClientApp/src/app/result/result-edit/result-edit.component.ts
+++ ClientApp/src/app/result/result-edit/result-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { ResultService } from '../result.service';
 
@@ -11,17 +12,28 @@ import { ResultService } from '../result.service';
 export class ResultEditComponent implements OnInit {
   title: string;
   result: Result;
+  form: FormGroup;
 
   // this will be TRUE when editing an existing result,
   // FALSE when creating a new one.
   editMode: boolean;
 
-  constructor(private activatedRoute: ActivatedRoute, private resultService: ResultService, private router: Router) {
+  constructor(private activatedRoute: ActivatedRoute,
+    private resultService: ResultService,
+    private fb: FormBuilder,
+    private router: Router) {
 
     // create an empty object from the Quiz interface
     this.result = <Result>{};
   }
   ngOnInit() {
+    // initialize the form
+    this.createForm();
+
+    this.loadData();
+
+  }
+  loadData() {
     const id = +this.activatedRoute.snapshot.params['id'];
 
     // check if we're in edit mode or not
@@ -34,15 +46,36 @@ export class ResultEditComponent implements OnInit {
       this.title = 'Create a new Result';
     }
   }
+  createForm() {
+    this.form = this.fb.group({
+      Text: ['', Validators.required],
+      MinValue: ['', Validators.pattern(/^\d*$/)],
+      MaxValue: ['', Validators.pattern(/^\d*$/)]
+    });
+  }
 
-  onSubmit(result: Result) {
+  updateForm() {
+    this.form.setValue({
+      Text: this.result.Text,
+      MinValue: this.result.MinValue || '',
+      MaxValue: this.result.MaxValue || ''
+    });
+  }
+  onSubmit() {
+    // build a temporary result object from form values
+    const tempResult = <Result>{};
+    tempResult.Text = this.form.value.Text;
+    tempResult.MinValue = this.form.value.MinValue;
+    tempResult.MaxValue = this.form.value.MaxValue;
+    tempResult.QuizId = this.result.QuizId;
+
     if (this.editMode) {
-      this.resultService.updateResult(result).subscribe(res => {
+      this.resultService.updateResult(tempResult).subscribe(res => {
         console.log('Result ' + res.Id + ' has been updated.');
         this.router.navigate(['quiz/edit', res.QuizId]);
       });
     } else {
-      this.resultService.addResult(result).subscribe(res => {
+      this.resultService.addResult(tempResult).subscribe(res => {
         console.log('Result ' + res.Id + ' has been updated.');
         this.router.navigate(['quiz/edit', res.QuizId]);
       });
@@ -51,5 +84,28 @@ export class ResultEditComponent implements OnInit {
 
   onBack() {
     this.router.navigate(['quiz/edit', this.result.QuizId]);
+  }
+
+  // retrieve a FormControl
+  getFormControl(name: string) {
+    return this.form.get(name);
+  }
+
+  // returns TRUE if the FormControl is valid
+  isValid(name: string) {
+    const e = this.getFormControl(name);
+    return e && e.valid;
+  }
+
+  // returns TRUE if the FormControl has been changed
+  isChanged(name: string) {
+    const e = this.getFormControl(name);
+    return e && (e.dirty || e.touched);
+  }
+
+  // returns TRUE if the FormControl is invalid after user changes
+  hasError(name: string) {
+    const e = this.getFormControl(name);
+    return e && (e.dirty || e.touched) && !e.valid;
   }
 }
--- ClientApp/src/app/result/result.module.ts
+++ ClientApp/src/app/result/result.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpModule } from '@angular/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { ResultListComponent } from './result-list/result-list.component';
 import { ResultEditComponent } from './result-edit/result-edit.component';
@@ -16,6 +16,7 @@ import { AnswerModule } from '../answer/answer.module';
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
 
     ResultRoutingModule,
