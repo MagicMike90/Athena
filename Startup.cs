09e1e6db24b5de615acc6ac11b97a395e9b5de35
Michael Luo <mluo@echoice.com.au>
2018-04-27 08:50:18 +1000

    enable server authentication with JWT tockes

--- Startup.cs
+++ Startup.cs
@@ -42,30 +42,30 @@ namespace Athena {
                 .AddEntityFrameworkStores<ApplicationDbContext> ();
 
             // Add Authentication with JWT Tokens
-            // services.AddAuthentication (opts => {
-            //         opts.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-            //         opts.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            //         opts.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            //     })
-            //     .AddJwtBearer (cfg => {
-            //         cfg.RequireHttpsMetadata = false;
-            //         cfg.SaveToken = true;
-            //         cfg.TokenValidationParameters = new TokenValidationParameters () {
-            //             // standard configuration
-            //             ValidIssuer = Configuration["Auth:Jwt:Issuer"],
-            //             IssuerSigningKey = new SymmetricSecurityKey (
-            //             Encoding.UTF8.GetBytes (Configuration["Auth:Jwt:Key"])),
-            //             ValidAudience = Configuration["Auth:Jwt:Audience"],
-            //             ClockSkew = TimeSpan.Zero,
+            services.AddAuthentication (opts => {
+                    opts.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                    opts.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    opts.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer (cfg => {
+                    cfg.RequireHttpsMetadata = false;
+                    cfg.SaveToken = true;
+                    cfg.TokenValidationParameters = new TokenValidationParameters () {
+                        // standard configuration
+                        ValidIssuer = Configuration["Auth:Jwt:Issuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey (
+                        Encoding.UTF8.GetBytes (Configuration["Auth:Jwt:Key"])),
+                        ValidAudience = Configuration["Auth:Jwt:Audience"],
+                        ClockSkew = TimeSpan.Zero,
 
-            //             // security switches
-            //             RequireExpirationTime = true,
-            //             ValidateIssuer = true,
-            //             ValidateIssuerSigningKey = true,
-            //             ValidateAudience = true
-            //         };
-            //         cfg.IncludeErrorDetails = true;
-            //     });
+                        // security switches
+                        RequireExpirationTime = true,
+                        ValidateIssuer = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidateAudience = true
+                    };
+                    cfg.IncludeErrorDetails = true;
+                });
 
         }
 
