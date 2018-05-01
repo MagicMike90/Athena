7272f384afdddd70e3b33d28835e8ab7aa964209
Michael Luo <mluo@echoice.com.au>
2018-05-01 16:41:00 +1000

    setup nginx docker seems having problem to connect to host machine

--- docker_ssl_proxy/docker_ssl_proxy.sh
+++ docker_ssl_proxy/docker_ssl_proxy.sh
@@ -1,1 +1,1 @@
-docker run --name nginx_proxy -d -v "$(pwd)":/etc/nginx/conf.d  -p 443:443 nginx
+docker run --add-host localnode:10.134.2.41 --name nginx_proxy -d -v "$(pwd)":/etc/nginx  -p 443:443  -p 80:80 nginx 
